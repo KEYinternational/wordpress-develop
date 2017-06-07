3f2bc9e22eee88a08d6fda4894743226acb2b791
Rafael Miranda <rafa8626@gmail.com>
2017-06-07 13:53:31 -0400

    Removed `loop` workaround not needed anymore

--- src/wp-includes/widgets/class-wp-widget-media-video.php
+++ src/wp-includes/widgets/class-wp-widget-media-video.php
@@ -118,10 +118,7 @@ class WP_Widget_Media_Video extends WP_Widget_Media {
 		if ( $attachment ) {
 			$src = wp_get_attachment_url( $attachment->ID );
 		} else {
-
-			// Manually add the loop query argument.
-			$loop = $instance['loop'] ? '1' : '0';
-			$src = empty( $instance['url'] ) ? $instance['url'] : add_query_arg( 'loop', $loop, $instance['url'] );
+		        $src = $instance['url'];
 		}
 
 		if ( empty( $src ) ) {
